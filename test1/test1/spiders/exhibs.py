efb3e4e419631738f9dc76e400647b1260fbade9
Alex Xie <babilun0723@gmail.com>
2018-02-27 23:27:42 +0800

    use xpath to parse exhibitor title and url

--- test1/test1/spiders/exhibs.py
+++ test1/test1/spiders/exhibs.py
@@ -2,57 +2,88 @@ import scrapy
 import uuid
 import urllib
 import io
+import logging
+import unicodedata
 from scrapy.spidermiddlewares.httperror import HttpError
 from twisted.internet.error import DNSLookupError
 from twisted.internet.error import TimeoutError, TCPTimedOutError
 from HTMLParser import HTMLParser
+from scrapy.utils.log import configure_logging
+
 
 class ExhibSpider(scrapy.Spider):
     name="exhibs"
     rooturl='http://www.eisenwarenmesse.com'
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(message)s')
+    file_handler = logging.FileHandler('exhibs.log')
+    file_handler.setFormatter(formatter)
+    
+    logger.addHandler(file_handler)
+    
+     
     h = HTMLParser()
 
-    start_urls = ['http://www.eisenwarenmesse.com/IEM/Exhibitor-search/index.php?fw_goto=aussteller/blaettern&&start=0&paginatevalues={"stichwort":""}']
+    # start_urls = ['http://www.eisenwarenmesse.com/IEM/Exhibitor-search/index.php?fw_goto=aussteller/blaettern&&start=0&paginatevalues={"stichwort":""}']
+    url_pattern = 'http://www.eisenwarenmesse.com/IEM/Exhibitor-search/index.php?fw_goto=aussteller/blaettern&&start=%s&paginatevalues={"stichwort":""}'
 
+    _urls = []
+    for i in range(0,20*137,20):
+        _urls.append(url_pattern % str(i))
+
+    start_urls = _urls
 
     def parse(self, response):
 
-        exhib_items_tags=response.xpath('//td[@class ="cspacer ca3"]/table/tr/td[1]/a').extract()
+        exhib_items_tags=response.xpath('//td[@class ="cspacer ca3"]/table/tr/td[1]/a')
 
         next_page = response.xpath('//div[@class="pagination xhidden-xs"]/div[@class="notmobile"]/a[last()]/@href').extract_first()
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
         
         for a in exhib_items_tags:
-            href = a.split('"')[1]
-            title = (a.split('>')[1]).split('<')[0]
-
-            if title is not None and len(title) > 0 :
-                title = self.h.unescape(title).strip()
-            else:
-                randomuid = str(uuid.uuid1())
-                title = "notitle_%s" % randomuid
+            uUrl = a.xpath('@href').extract_first() 
+            uTitle = a.xpath('text()').extract_first() 
             
-            href = self.h.unescape(href)
-            href = '%s%s' % (self.rooturl, href)
+            nUrl = unicodedata.normalize('NFKD', uUrl).encode('ascii','ignore')
+            nUrl = '%s%s' %(self.rooturl, nUrl)
 
-            request = scrapy.Request(href, callback=self.parse_exhibitor, errback = self.errback_httpbin)
-            request.meta["title"] = title
+            nTitle = (unicodedata.normalize('NFKD', uTitle).encode('ascii','ignore')).strip()
+            
+            self.logger.info('%s  %s', nTitle, nUrl )
+
+            
+
+            # href = a.split('"')[1]
+            # title = (a.split('>')[1]).split('<')[0]
+
+            # if title is not None and len(title) > 0 :
+            #     title = self.h.unescape(title).strip()
+            # else:
+            #     randomuid = str(uuid.uuid1())
+            #     title = "notitle_%s" % randomuid
+            
+            # href = self.h.unescape(href)
+            # href = '%s%s' % (self.rooturl, href)
+            # self.logger.info('%s %s', title, href )
+            request = scrapy.Request(nUrl, callback=self.parse_exhibitor, errback = self.errback_httpbin)
+            request.meta["title"] = nTitle
             yield request
-
+            
     def parse_exhibitor(self, response):
         title = response.meta["title"]
         filename = 'htmlpages/%s.html' % title.replace("/"," ")
 
-        html = response.xpath('//div[@class="maincontent"]').extract_first()
+        html = response.xpath('//div[@class="platzFuerCross"]').extract_first()
         
         if len(str(html)) > 4:
             with io.open(filename, 'wb') as f:
                 f.write(str(html))
-                self.log('Saved file %s' % filename)
+                self.logger.info('Saved file %s', filename)
         else:
-            self.log('Nothing is captured in %s' % response.url)
-            self.log('Trying to recrawl %s' % response.url)
             request = scrapy.Request(response.url, callback=self.parse_exhibitor, errback = self.errback_httpbin)
             request.meta["title"] = title
             yield request
